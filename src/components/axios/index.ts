f4e6dd0284f92ecd5bee011f0b012a845ac62796
Gilmar Penido <gilmar.penido@gmail.com>
2025-07-16 08:14:19 -0300

    alterando url padrao

--- src/components/axios/index.ts
+++ src/components/axios/index.ts
@@ -1,10 +1,5 @@
 import axios from 'axios';
 
-const api = axios.create({
-    //url mgcloud
-    //baseURL: 'http://192.91.254.124:55081/',
-    //url local
-    baseURL: 'http://localhost:3000/',
-});
+const api = axios.create();
 
 export default api;
